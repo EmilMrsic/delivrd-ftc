96b239464b6f1bed3ad1251fef17ae7fb7d681e7
thehexatown-tabarak-sohail <tabarak.sohail@thehexatown.com>
2024-11-20 04:51:15 +0500

    feat: dealer-id-instead-of-user-id

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@ export interface IUser {
   trade_details: string;
   revenue: number;
   is_onboarding_complete: boolean | null;
+  dealer_id: string[];
   total_budget: number[];
   privilege: string[];
   email: string;
