45a09baeefed581e70c39e45c03db8e4a00626d7
Haider Ali <61574206+itshaider01@users.noreply.github.com>
2024-11-22 12:23:06 +0500

    feat: login link with our domain

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,6 @@
-import { auth } from "@/firebase/config";
-import { sendSignInLinkToEmail } from "firebase/auth";
+
+import { initAdmin } from "@/firebaseAdmin";
+import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -8,15 +9,53 @@ export async function POST(req: Request) {
   const protocol = req.headers.get("x-forwarded-proto") || "http";
   const host = req.headers.get("host");
   const baseUrl = `${protocol}://${host}`;
+
   try {
     const actionCodeSettings = {
       url: `${baseUrl}/complete-signin?email=${to}`,
       handleCodeInApp: true, // Important for email link sign-in
     };
 
-    console.log({ actionCodeSettings });
-    await sendSignInLinkToEmail(auth, to, actionCodeSettings);
+    const admin = await initAdmin();
+    const auth = admin.auth();
+    const link = await auth.generateSignInWithEmailLink(to, actionCodeSettings);
 
+    const subject = "Delivrd Login Link";
+    const html =
+      `<!DOCTYPE html>
+  <html>
+  <body>
+  <p>Hey 👋,</p>
+  <p>
+    Use <a href="${link}">this</a> link to Log in.
+  </p>
+  <p>Do not reply to this email, it is an automated system that does not receive emails.</p>
+  </body>
+  </html>`;
+
+    const emailData = new URLSearchParams();
+    emailData.append(
+      "from",
+      `Delivrd <postmaster@${process.env.NEXT_PUBLIC_MAILGUN_DOMAIN_NAME}>`,
+    );
+    emailData.append("to", to);
+    emailData.append("subject", subject);
+    emailData.append("html", html);
+
+    const response = await axios.post(
+      `https://api.mailgun.net/v3/${process.env.NEXT_PUBLIC_MAILGUN_DOMAIN_NAME}/messages`,
+      emailData,
+      {
+        auth: {
+          username: "api",
+          password: process.env.NEXT_PUBLIC_MAILGUN_API_KEY || "",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    );
+    console.log(response.data)
     return NextResponse.json({ message: "Email sent successfully!" });
   } catch (error) {
     console.error("Error sending email:", error);
