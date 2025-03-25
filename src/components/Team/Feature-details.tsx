0ad0bfbeb1994abb99824d177ada80b8b48bc7e4
Dylan Holland <dylaneholland@gmail.com>
2025-03-25 11:38:16 -0700

    dylan/profile-updates:

--- src/components/Team/Feature-details.tsx
+++ src/components/Team/Feature-details.tsx
@@ -178,7 +178,7 @@ const FeatureDetails = ({
           icon={Car}
         />
 
-        {/* <InputField
+        <InputField
           label="Trade In"
           value={negotiation?.tradeInInfo ?? "Trade in not available"}
           negotiationId={negotiationId ?? ""}
@@ -199,7 +199,7 @@ const FeatureDetails = ({
               <path d="M8 5a1 1 0 100 2h5.586l-1.293 1.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L13.586 5H8zM12 15a1 1 0 100-2H6.414l1.293-1.293a1 1 0 10-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L6.414 15H12z" />
             </svg>
           )}
-        /> */}
+        />
         <InputField
           options={["Lease", "Cash", "Finance", "Need to Discuss"]}
           label="Finance Type"
